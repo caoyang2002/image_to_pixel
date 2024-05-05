7e67f6934d1c09fa1cf5762123d500fe4985ee24
caoyang2002 <cy12968@163.com>
2024-05-05 23:38:07 +0800

    fix readme

--- conf.py
+++ conf.py
@@ -15,24 +15,39 @@ ROTATIONS = [0]
 
 # number of divisions per channel
 # (COLOR_DEPTH = 32 -> 32 * 32 * 32 = 32768 colors)
+# 每个颜色通道的划分数
 COLOR_DEPTH = 32
 # Scale of the image to be tiled (1 = default resolution)
+# 要平铺的图像的缩放比例（1 = 默认分辨率）
 IMAGE_SCALE = 1
 # tiles scales (1 = default resolution)
+# 瓦片的缩放比例（1 = 默认分辨率）
 RESIZING_SCALES = [0.5, 0.4, 0.3, 0.2, 0.1]
 # number of pixels shifted to create each box (tuple with (x,y))
 # if value is None, shift will be done accordingly to tiles dimensions
-PIXEL_SHIFT = (5, 5)
+# 创建每个盒子时移动的像素数（(x,y) 的元组）
+# 如果值为 None，则根据瓦片的尺寸进行移动
+# PIXEL_SHIFT = (5, 5)
+PIXEL_SHIFT = None
 # if tiles can overlap
+# 瓦片是否可以重叠
 OVERLAP_TILES = False
+# OVERLAP_TILES = True
 # render image as its being built
+# 渲染图像时边构建边显示
 RENDER = False
+# RENDER = True
 # multiprocessing pool size
+# 多进程池的大小
 POOL_SIZE = 8
 
 # out file name
 OUT = 'out.png'
 # image to tile (ignored if passed as the 1st arg)
-IMAGE_TO_TILE = None
+# IMAGE_TO_TILE = None
+IMAGE_TO_TILE = r"image_in/bird_x.png"
+# IMAGE_TO_TILE = r"image_in/bird_x.png"
 # folder with tiles (ignored if passed as the 2nd arg)
-TILES_FOLDER = None
+# TILES_FOLDER = None
+# TILES_FOLDER = r"tiles/lego/gen_lego_v"
+TILES_FOLDER = r"tiles/block/gen_block"
--- tiler.py
+++ tiler.py
@@ -64,13 +64,14 @@ def mode_color(img):
         for x in y:
             # 如果像素的alpha通道不为0，或者图像没有alpha通道（即长度小于4）
             if len(x) < 4 or x[3] != 0:
-                # 将RGB值作为键，增加计数
+                # (x:[:3]) 表示将 RG B值作为键，并增加计数
                 counter[tuple(x[:3])] += 1
             else:
-                # 记录完全透明的像素
+                # 记录为完全透明的像素
                 counter[(-1, -1, -1)] += 1
             # 增加总像素数
             total += 1
+    # print("The total number of pixels is: " ,total)
     # 如果有非透明像素
     if total > 0:
         # 找到出现次数最多的颜色
@@ -102,11 +103,12 @@ def show_image(img, wait=True):
 
 
 # load and process the tiles
-# 加载和处理瓦片的函数
+# 加载和处理像素块的函数
 def load_tiles(paths):
-    # 打印加载瓦片的提示信息
+    # 打印加载像素块的提示信息
     print('Loading tiles')
     # 使用defaultdict，便于向tiles中添加瓦片数据
+    # defaultdict 会为每个新键自动创建一个空的列表（list）
     tiles = defaultdict(list)
 
     # 对于传入的每个路径
@@ -115,26 +117,26 @@ def load_tiles(paths):
         if os.path.isdir(path):
             # 遍历目录下的所有文件
             for tile_name in tqdm(os.listdir(path)):
-                # 读取每个瓦片图像
+                # 读取每个像素块图像
                 tile = read_image(os.path.join(path, tile_name))
-                # 获取瓦片的主要颜色和相对频率
+                # 获取像素块的主要颜色和相对频率
                 mode, rel_freq = mode_color(tile)
                 # 如果获取到的主要颜色不是None
                 if mode != None:
                     # 遍历所有的缩放比例
                     for scale in RESIZING_SCALES:
-                        # 根据比例缩放瓦片
+                        # 根据比例缩放像素块
                         t = resize_image(tile, scale)
-                        # 获取缩放后瓦片的分辨率
+                        # 获取缩放后瓦片的分辨率, 取出 t.shape 元组的前两个元素，也就是图像的高度和宽度。
                         res = tuple(t.shape[:2])
-                        # 将缩放后的瓦片添加到tiles字典中
+                        # 将缩放后的像素块添加到 tiles 字典中
                         tiles[res].append({
-                            'tile': t,
-                            'mode': mode,
-                            'rel_freq': rel_freq
+                            'tile': t, # 像素块
+                            'mode': mode, # 主要颜色
+                            'rel_freq': rel_freq  # 主要颜色在图像像素块中出现的相对频率
                         })
 
-            # 将tiles数据持久化存储到pickle文件
+            # 将tiles数据存储到pickle文件
             with open('tiles.pickle', 'wb') as f:
                 # 使用pickle.dump序列化tiles对象并写入文件
                 pickle.dump(tiles, f)
@@ -146,7 +148,7 @@ def load_tiles(paths):
             with open(path, 'rb') as f:
                 # 使用pickle.load反序列化文件内容到tiles变量
                 tiles = pickle.load(f)
-    # 返回tiles对象，包含了所有加载和处理后的瓦片数据
+    # 返回 tiles 对象，包含了所有加载和处理后的瓦片数据
     return tiles
 
 
@@ -191,26 +193,26 @@ def color_distance(c1, c2):
 
 
 # returns the most similar tile to a box (in terms of color)
-# 返回与一个盒子在颜色上最相似的瓦片
+# 返回与一个方块在颜色上最相似的像素块
 def most_similar_tile(box_mode_freq, tiles):
-    # 如果盒子的主要颜色不存在
+    # 如果方块的主要颜色不存在
     if not box_mode_freq[0]:
         # 返回零距离和空白图像
         return (0, np.zeros(shape=tiles[0]['tile'].shape))
     else:
-        # 初始化最小距离为None
+        # 初始化最小相似度距离为 None
         min_distance = None
-        # 初始化最相似瓦片图像为None
+        # 初始化最相似像素块图像为 None
         min_tile_img = None
-        # 对于tiles列表中的每个瓦片
+        # 遍历像素块列表中的每个像素块
         for t in tiles:
-            # 计算与盒子颜色的相似度距离
+            # 计算一个距离, 标识像素块与方块颜色的相似度
             dist = (1 + color_distance(box_mode_freq[0], t['mode'])) / box_mode_freq[1]
             # 如果当前距离更小
             if min_distance == None or dist < min_distance:
                 # 更新最小距离
                 min_distance = dist
-                # 更新最相似的瓦片图像
+                # 更新最相似的像素块图像
                 min_tile_img = t['tile']
         # 返回最小距离和最相似的瓦片图像
         return (min_distance, min_tile_img)
@@ -220,7 +222,7 @@ def most_similar_tile(box_mode_freq, tiles):
 # 构建盒子并为每个盒子找到最佳瓦片
 def get_processed_image_boxes(image_path, tiles):
     # 打印提示信息
-    print('Gettting and processing boxes')
+    print('Getting and processing boxes')
     # 读取图像
     img = read_image(image_path)
     # 创建多进程池
@@ -230,37 +232,37 @@ def get_processed_image_boxes(image_path, tiles):
 
     # 对tiles字典按键（分辨率）降序排序后进行遍历
     for res, ts in tqdm(sorted(tiles.items(), reverse=True)):
-        # 为当前分辨率生成所有盒子
+        # 为当前分辨率生成所有方块
         boxes = image_boxes(img, res)
-        # 多进程计算每个盒子的主要颜色
+        # 多进程计算每个方块的主要颜色
         modes = pool.map(mode_color, [x['img'] for x in boxes])
-        # 多进程找到每个盒子最相似的瓦片
+        # 多进程找到每个方块最相似的瓦片
         most_similar_tiles = pool.starmap(most_similar_tile, zip(modes, [ts for x in range(len(modes))]))
 
-        # 初始化盒子索引
+        # 初始化方块索引
         i = 0
-        # 对于每个最相似的瓦片
+        # 遍历每个最相似的像素块
         for min_dist, tile in most_similar_tiles:
-            # 更新盒子的最小距离
+            # 更新方块的最小距离
             boxes[i]['min_dist'] = min_dist
-            # 更新盒子的最相似瓦片
+            # 更新方块的最相似像素块
             boxes[i]['tile'] = tile
-            # 盒子索引加1
+            # 方块索引加 1
             i += 1
-        # 将当前分辨率的所有盒子添加到总列表中
+        # 将当前分辨率的所有方块添加到总列表中
         all_boxes += boxes
-    # 返回所有盒子和图像的形状
+    # 返回所有方块和图像的形状
     return all_boxes, img.shape
 
 
 # places a tile in the image
-# 将瓦片放置到图像中
+# 将像素块放置到图像中
 def place_tile(img, box):
-    # 获取盒子的起始位置（翻转位置元组）
+    # 获取方块的起始位置（翻转位置元组）
     p1 = np.flip(box['pos'])
-    # 获取盒子的结束位置（起始位置+盒子图像的尺寸）
+    # 获取方块的结束位置（起始位置+方块图像的尺寸）
     p2 = p1 + box['img'].shape[:2]
-    # 创建一个布尔掩码，其中alpha通道非零的地方为True
+    # 创建一个布尔掩码，其中alpha通道非零的地方为 True
     mask = box['tile'][:, :, 3] != 0
     # 获取当前盒子在原图中的位置对应的图像区域
     img_box = img[p1[0]:p2[0], p1[1]:p2[1]]
@@ -273,23 +275,42 @@ def place_tile(img, box):
 
 
 # tiles the image
-# 使用给定的盒子集合创建平铺图像
+# 使用给定的方块集合创建像素化风格的图像
 def create_tiled_image(boxes, res, render=False):
     # 打印提示信息，开始创建平铺图像
     print('Creating tiled image')
-    # 创建一个黑色的空白图像数组，4表示RGBA颜色通道
+    # 创建一个黑色的空白图像数组，4 表示 RGBA 颜色通道
     img = np.zeros(shape=(res[0], res[1], 4), dtype=np.uint8)
 
-    # 遍历盒子列表，盒子根据最小距离排序，如果允许瓦片重叠，则逆序排列
-    for box in tqdm(sorted(boxes, key=lambda x: x['min_dist'], reverse=OVERLAP_TILES)):
-        # 将当前盒子的瓦片放置到图像中
+    # 遍历方块列表，方块根据最小距离排序，如果允许像素块重叠，则逆序排列
+    for box in tqdm(sorted(boxes, key=lambda x: x['min_dist'], reverse=OVERLAP_TILES)): # 根据每个方块字典中的 'min_dist' 键的值来排序
+        # 确保box['tile']是RGBA格式
+        if box['tile'].ndim == 2:
+            # 将RGB二维数组转换为RGBA，alpha通道全为255
+            box['tile'] = np.dstack(
+                (box['tile'], np.ones((box['tile'].shape[0], box['tile'].shape[1]), dtype=np.uint8) * 255))
+        elif box['tile'].ndim == 3:
+            if box['tile'].shape[2] == 3:
+                # 将RGB转换为RGBA，alpha通道全为255
+                box['tile'] = np.concatenate(
+                    (box['tile'], np.ones((box['tile'].shape[0], box['tile'].shape[1], 1), dtype=np.uint8) * 255),
+                    axis=2)
+            elif box['tile'].shape[2] != 4:
+                print("Error: A tile does not have the correct dimensions for an alpha channel.")
+                sys.exit(1)
+
+        # 现在box['tile']确保是RGBA格式，可以安全地创建mask
+        # mask = box['tile'][:, :, 3] != 0
+
+
+        # 将当前方块的像素块放置到图像中
         place_tile(img, box)
         # 如果设置为渲染模式
         if render:
             # 显示当前的平铺图像
             show_image(img, wait=False)
             # 暂停0.025秒，以便观察
-            sleep(0.025)
+            sleep(0.001)
     # 返回最终的平铺图像
     return img
 
@@ -297,36 +318,36 @@ def create_tiled_image(boxes, res, render=False):
 # main
 # 主函数
 def main():
-    # 检查命令行参数，获取要平铺的图像路径
+    # 检查命令行参数，获取要改为像素风格的图像路径
     if len(sys.argv) > 1:
         image_path = sys.argv[1]
     else:
         # 从配置文件中获取图像路径
         image_path = conf.IMAGE_TO_TILE
-    # 检查命令行参数，获取瓦片路径列表
+    # 检查命令行参数，获取像素块路径列表
     if len(sys.argv) > 2:
-        # 从命令行参数中获取瓦片路径
+        # 从命令行参数中获取像素块的路径
         tiles_paths = sys.argv[2:]
     else:
-        # 从配置文件中获取瓦片路径列表
+        # 从配置文件中获取瓦片路径列表 (默认配置, 需要先在配置文件中配置)
         tiles_paths = conf.TILES_FOLDER.split(' ')
 
-    # 检查图像路径是否存在
+    # 检查带更改的图像路径是否存在
     if not os.path.exists(image_path):
         # 如果不存在，打印错误信息并退出程序
         print('Image not found')
         exit(-1)
-    # 检查所有瓦片路径是否存在
+    # 检查所有像素块路径是否存在
     for path in tiles_paths:
         if not os.path.exists(path):
-            # 如果瓦片路径不存在，打印错误信息并退出程序
+            # 如果像素块路径不存在，打印错误信息并退出程序
             print('Tiles folder not found')
             exit(-1)
-    # 加载和处理瓦片
+    # 加载和处理像素块, 主要是获取缩放, 然后保存在缓存文件里
     tiles = load_tiles(tiles_paths)
-    # 获取处理后的盒子和原始图像分辨率
+    # 获取处理后的方块和原始图像分辨率
     boxes, original_res = get_processed_image_boxes(image_path, tiles)
-    # 使用盒子创建平铺图像
+    # 使用方块创建像素风格图像
     img = create_tiled_image(boxes, original_res, render=conf.RENDER)
     # 将最终的平铺图像写入文件
     cv2.imwrite(conf.OUT, img)
